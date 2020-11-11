2bfec79700f4b47e5a85f1b0d8009177c61ecae7
jonathan jorge <jonathanjorge8445@gmail.com>
2020-11-11 18:55:54 -0300

    🎨 #1 adding accent remover

--- organize-dataset.py
+++ organize-dataset.py
@@ -1,7 +1,18 @@
 import pandas as pd
+from unicodedata import normalize
+def accent_remover(x):
 
-data_set2017 = pd.read_csv('datasets/datatran2017.csv', sep= ';', encoding='ISO-8859-1').dropna().drop_duplicates()
-data_set2018 = pd.read_csv('datasets/datatran2018.csv', sep= ';', encoding='ISO-8859-1').dropna().drop_duplicates()
-data_set2019 = pd.read_csv('datasets/datatran2019.csv', sep= ';', encoding='ISO-8859-1').dropna().drop_duplicates()
-dataset_train = pd.concat([data_set2017,data_set2018,data_set2019])
-    
+    try:
+        float(x)
+        return x
+    except:
+        return normalize('NFKD',x).encode('ASCII', 'ignore').decode('ASCII')
+
+dataset_2017 = pd.read_csv('datasets/datatran2017.csv', sep= ';', encoding='ISO-8859-1').dropna().drop_duplicates()
+dataset_2018 = pd.read_csv('datasets/datatran2018.csv', sep= ';', encoding='ISO-8859-1').dropna().drop_duplicates()
+dataset_2019 = pd.read_csv('datasets/datatran2019.csv', sep= ';', encoding='ISO-8859-1').dropna().drop_duplicates()
+dataset = pd.concat([dataset_2017,dataset_2018,dataset_2019])
+
+dataset = dataset.transform([accent_remover])
+dataset.columns = dataset.columns.droplevel(1)
+
