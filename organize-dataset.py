8618bd42dd68c7fab0beab2146aaf2c548669248
Jonathan-Oliveira <jonathan.jb.olivera@gmail.com>
2020-11-17 22:27:37 -0300

    Revert ":memo: #1 update organize dataset"
    This reverts commit e9b62feae2710b783d08fb651ebb48cd9f14e2c8.

--- organize-dataset.py
+++ organize-dataset.py
@@ -1,8 +1,18 @@
 import pandas as pd
+from unicodedata import normalize
+def accent_remover(x):
 
-dataset_2017 = pd.read_csv('datasets/datatran2017.csv', sep= ';', encoding='ISO-8859-1', header=0 , infer_datetime_format=True, parse_dates=['data_inversa'], index_col=['data_inversa']).dropna().drop_duplicates()
-dataset_2018 = pd.read_csv('datasets/datatran2018.csv', sep= ';', encoding='ISO-8859-1', header=0 , infer_datetime_format=True, parse_dates=['data_inversa'], index_col=['data_inversa']).dropna().drop_duplicates()
-dataset_2019 = pd.read_csv('datasets/datatran2019.csv', sep= ';', encoding='ISO-8859-1', header=0 , infer_datetime_format=True, parse_dates=['data_inversa'], index_col=['data_inversa']).dropna().drop_duplicates()
-dataset_2020 = pd.read_csv('datasets/datatran2020.csv', sep= ';', encoding='ISO-8859-1', header=0 , infer_datetime_format=True, parse_dates=['data_inversa'], index_col=['data_inversa']).dropna().drop_duplicates()
-dataset_train = pd.concat([dataset_2017,dataset_2018,dataset_2019])
-dataset_test = dataset_2020
+    try:
+        float(x)
+        return x
+    except:
+        return normalize('NFKD',x).encode('ASCII', 'ignore').decode('ASCII')
+
+dataset_2017 = pd.read_csv('datasets/datatran2017.csv', sep= ';', encoding='ISO-8859-1').dropna().drop_duplicates()
+dataset_2018 = pd.read_csv('datasets/datatran2018.csv', sep= ';', encoding='ISO-8859-1').dropna().drop_duplicates()
+dataset_2019 = pd.read_csv('datasets/datatran2019.csv', sep= ';', encoding='ISO-8859-1').dropna().drop_duplicates()
+dataset = pd.concat([dataset_2017,dataset_2018,dataset_2019])
+
+dataset = dataset.transform([accent_remover])
+dataset.columns = dataset.columns.droplevel(1)
+
